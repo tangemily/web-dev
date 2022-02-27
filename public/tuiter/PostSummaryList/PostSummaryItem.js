0d036d79a37c9d8e40821226eb023a16fe1ab438
tangemily <tang.s.emily@gmail.com>
2022-02-26 22:52:44 -0500

    use css file instead of overriding style in div

--- public/tuiter/PostSummaryList/PostSummaryItem.js
+++ public/tuiter/PostSummaryList/PostSummaryItem.js
@@ -1,8 +1,7 @@
 const PostSummaryItem = (post) => {
     return (`
             <li class="list-group-item align-items-center justify-content-around flex-row d-flex">
-                <div class="float-left text-wrap wd-explore-text"
-                     style="width:90%">
+                <div class="float-left text-wrap wd-explore-text">
                     <div class="text-muted">${post.topic}</div>
                     <b>${post.userName}</b>
                     <span class="text-muted">- ${post.time}</span><br>
