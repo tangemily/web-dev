d545c028d48c5bdefa1131198a5b9cdaca0b91e3
tangemily <tang.s.emily@gmail.com>
2022-04-14 16:42:19 -0400

    dislike functionality

--- src/components/tuiter/tuit-list/tuit-stats.js
+++ src/components/tuiter/tuit-list/tuit-stats.js
@@ -24,7 +24,13 @@ const TuitStats = ({tuit}) => {
                     likes: tuit.likes + 1
                 })} className="far fa-thumbs-up ms-2"></i>
             </div>
-
+            <div>
+                Dislikes: {tuit.dislikes}
+                <i onClick={() => updateTuit(dispatch, {
+                    ...tuit,
+                    dislikes: tuit.dislikes + 1
+                })} className="far fa-thumbs-down ms-2"></i>
+            </div>
             <div className="col">
                 <i className="fas fa-external-link-alt me-2"></i>
             </div>
